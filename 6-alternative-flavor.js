49516c6e8a6cf3f637e2ff2a2f46b54bea546b0c
PatrickChewing8 <84030379+PatrickChewing8@users.noreply.github.com>
2022-06-25 12:53:45 -0400

    Initial commit

--- 6-alternative-flavor.js
+++
@@ -1,4 +0,0 @@
-module.exports.item = ['item1', 'item2'];
-const person = {
-	name: 'bob',
-};
--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const _ = require('lodash');
+const express = require('express');
+const app = express();
+const people = require('./routes/people');
+const auth = require('./routes/auth');
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+// static assets
+app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+
+app.use('/api/people', people);
+app.use('/login', auth);
+
+app.listen(5000, () => {
+	console.log('Server is listening on port 5000....');
+});
